bfd40df267f4047d549d9d190d333d59b87d7d40
Jonathan Le <jonathanle1111@gmail.com>
2023-01-28 15:39:58 -0600

    UI-react

---
+++ react-frontend/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+
+
+      <div className>
+      
+      <div className='roundedSquare'>
+
+      <p>Hello</p>
+
+
+
+
+      </div>
+
+      </div>
+
+    );
+  }
+}
+
+export default App;

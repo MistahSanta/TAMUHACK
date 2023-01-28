e98aa6213b8e2b6cc455a31b2e018e7ec47c68f2
BenWowo <111255005+BenWowo@users.noreply.github.com>
2023-01-28 15:48:53 -0600

    created page skeleton

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -5,19 +5,18 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      <div className="rounded_square">
+        <nav className="top-bar"></nav>
+        <div className="main">
+          <div className="flex-row">
+            <nav className="sidebar">
 
-
-      <div className>
-      
-      <div className='roundedSquare'>
-
-      <p>Hello</p>
-
-
-
-
-      </div>
-
+            </nav>
+            <div className="main-content">
+              
+            </div>
+          </div>
+        </div>
       </div>
 
     );

efdb1324021c28c096939e384b0788cd21e566d1
BenWowo <111255005+BenWowo@users.noreply.github.com>
2023-01-28 16:21:58 -0600

    added some css and img

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="rounded_square">
+      <div className="rounded-square">
         <nav className="top-bar">
-          This is top bar contentn
+          <span>CBRE</span>
+          <span>Dashboard</span>
+          <img src="src\assets\settings-gear.svg" alt="" />
         </nav>
         <div className="main">
           <div className="flex-row">
             <nav className="sidebar">
-
+                <span>Dashboard</span>
+                <span>teams</span>
+                <span>insights</span>
             </nav>
+
+            {/*This is jonathan's part */}
             <div className="main-content">
+            Testing my part
 
             </div>
           </div>
